abdcaf94f97f562c4bc73ef734735be0bb2d4ad8
mosetf <stanlym315@gmail.com>
2023-06-22 23:18:05 +0300

    main.c

---
+++ san.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+void swap(stack_t **stack, unsigned int line_number)
+{
+    int temp;
+
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    temp = (*stack)->n;
+    (*stack)->n = (*stack)->next->n;
+    (*stack)->next->n = temp;
+}
+
+void add(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    (*stack)->next->n += (*stack)->n;
+    pop(stack, line_number);
+}
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+}
